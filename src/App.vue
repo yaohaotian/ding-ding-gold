3e0421f0921ffa07e7162eb4c93c8aabbe863918
yaohaotian <783441251@qq.com>
2025-01-03 11:25:43 +0800

    less引入

--- src/App.vue
+++ src/App.vue
@@ -6,18 +6,12 @@ import TheWelcome from '@/components/TheWelcome.vue'
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-  <main>
-    <TheWelcome />
-  </main>
+  <div>
+    
+  </div>
 </template>
 
-<style scoped>
+<style lang="less" scoped>
 header {
   line-height: 1.5;
 }
--- vite.config.ts
+++ vite.config.ts
@@ -8,6 +8,18 @@ import NutUIResolver from '@nutui/auto-import-resolver'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  css: {
+    preprocessorOptions: {
+      less: {
+        // 在这里你可以配置一些 Less 的选项，比如：
+        modifyVars: {
+          // 例如修改 Ant Design 的主题色
+          'primary-color': '#1DA57A'
+        },
+        javascriptEnabled: true // 启用 JavaScript 功能
+      }
+    }
+  },
   plugins: [
     vue(),
     vueJsx(),
