92772f09203d20d5c2e8d4e48946a376ae5d269e
yaohaotian <783441251@qq.com>
2025-01-03 11:22:11 +0800

    nutui引入

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,17 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 
+import {
+  showToast,
+  showNotify,
+  showDialog,
+  showImagePreview
+} from '@nutui/nutui'
+
 import './assets/main.css'
+import '@nutui/nutui/dist/packages/toast/style/css'
+import '@nutui/nutui/dist/packages/notify/style/css'
+import '@nutui/nutui/dist/packages/dialog/style/css'
+import '@nutui/nutui/dist/packages/imagepreview/style/css'
 
 createApp(App).mount('#app')
--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,20 @@
 import { fileURLToPath, URL } from 'node:url'
-
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
 
+import Components from 'unplugin-vue-components/vite'
+import NutUIResolver from '@nutui/auto-import-resolver'
+
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), vueJsx(),],
+  plugins: [
+    vue(),
+    vueJsx(),
+    Components({
+      resolvers: [NutUIResolver()]
+    })
+  ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
